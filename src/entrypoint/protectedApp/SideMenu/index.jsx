1fbfc17d3438e570e4fe1d185ea7a431de4a108a
Bumble- <ibrahim.drissi@x-hub.io>
2022-10-25 23:33:04 +0100

    create profile page

--- src/entrypoint/protectedApp/SideMenu/index.jsx
+++ src/entrypoint/protectedApp/SideMenu/index.jsx
@@ -1,58 +1,74 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { sideMenuConsts } from "../../../utils/constants";
-import { Routing } from "../../Routes";
-import {
-  FaCog,
-  FaUserAlt,
-  FaChartBar,
-  FaProjectDiagram,
-  FaFileAlt,
-  FaUserCircle,
-  FaGraduationCap,
-  FaNewspaper,
-} from "react-icons/fa";
+import { BsChevronDoubleLeft } from "react-icons/bs";
+import { menuItems } from "../../../core/data/SideMenu";
+import { DarkModeContext } from "../../../App";
 
 const SideMenu = () => {
+  const [isMenuCollapsed, setIsMenuCollapsed] = React.useState(false);
+  const isDarkMode = React.useContext(DarkModeContext);
   return (
-    <div className="side-menu-bar w-44 h-screen text-center">
-      <div className="logo p-4">
-        <h1 className="font-medium text-lg">Elzero</h1>
+    <div
+      className={`${
+        isDarkMode ? "bg-secondary-dark" : "bg-white"
+      } side-menu-bar w-fit h-screen text-center shadow-md
+      shadow-gray-500 z-10 px-2 flex flex-col justify-between pt-4 pb-4`}
+    >
+      <div>
+        <div className="logo py-4">
+          <h1
+            className={`${
+              isDarkMode ? "text-gray-300" : "text-black"
+            } font-medium text-lg`}
+          >
+            Elzero
+          </h1>
+        </div>
+        <ul className="side-menu-paths flex flex-col items-center !p-3 mt-3">
+          {menuItems?.map((elem) => {
+            const Icon = elem?.icon;
+            return (
+              <li
+                className="flex items-center w-full gap-4 py-1 px-2 mb-4 h-6"
+                key={elem.id}
+              >
+                <Link
+                  className="w-full flex text-sm items-center gap-4"
+                  to={elem?.link}
+                >
+                  <Icon
+                    className={isDarkMode ? "text-gray-300" : "text-black"}
+                  />
+                  <span
+                    className={`${isMenuCollapsed ? "hidden" : "block"} ${
+                      isDarkMode ? "text-gray-300" : "text-black"
+                    }`}
+                  >
+                    {elem?.name}
+                  </span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-      <ul className="side-menu-paths flex flex-col items-center h-screen p-3 mt-3">
-        <li className="flex items-center w-full gap-4 py-1 px-2 mb-4">
-          <FaChartBar />
-          <Link to={Routing.app.path}>{sideMenuConsts.dashboard}</Link>
-        </li>
-        <li className="flex items-center w-full gap-4 py-1 px-2 mb-4">
-          <FaCog />
-          <Link to={Routing.settings.path}>{sideMenuConsts.settings}</Link>
-        </li>
-        <li className="flex items-center w-full gap-4 py-1 px-2 mb-4">
-          <FaUserAlt />
-          <Link to={Routing.profile.path}>{sideMenuConsts.profile}</Link>
-        </li>
-        <li className="flex items-center w-full gap-4 py-1 px-2 mb-4">
-          <FaProjectDiagram />
-          <Link to={Routing.projects.path}>{sideMenuConsts.projects}</Link>
-        </li>
-        <li className="flex items-center w-full gap-4 py-1 px-2 mb-4">
-          <FaGraduationCap />
-          <Link to={Routing.courses.path}>{sideMenuConsts.courses}</Link>
-        </li>
-        <li className="flex items-center w-full gap-4 py-1 px-2 mb-4">
-          <FaUserCircle />
-          <Link to={Routing.friends.path}>{sideMenuConsts.friends}</Link>
-        </li>
-        <li className="flex items-center w-full gap-4 py-1 px-2 mb-4">
-          <FaFileAlt />
-          <Link to={Routing.files.path}>{sideMenuConsts.files}</Link>
-        </li>
-        <li className="flex items-center w-full gap-4 py-1 px-2 mb-4">
-          <FaNewspaper />
-          <Link to={Routing.plans.path}>{sideMenuConsts.plans}</Link>
-        </li>
-      </ul>
+
+      <button
+        className={`${
+          isDarkMode ? "text-white" : "text-black"
+        } w-full hover:bg-black hover:bg-opacity-20 py-2 rounded transition-colors flex items-center justify-evenly`}
+        onClick={() => setIsMenuCollapsed((prev) => !prev)}
+        type="button"
+      >
+        <BsChevronDoubleLeft
+          className={`${
+            isMenuCollapsed ? "rotate-180" : "rotate-0"
+          } transition-all`}
+        />
+        <span className={`${isMenuCollapsed ? "hidden" : "block"} text-sm`}>
+          Collapse
+        </span>
+      </button>
     </div>
   );
 };
